71df160bbe0ab402bd168026a24738eb19cebec4
AnatoliySt20 <strikun.a.p@gmail.com>
2017-10-10 20:50:16 +0300

    1

--- meetings/meetings/settings.py
+++ meetings/meetings/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'meetings.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres_db',
+        'USER' : 'postgres',
+        'PASSWORD' : '1505493',
+        'HOST' : 'localhost',
+        'PORT' : '5433',
     }
 }
 
