6fe32c237036358522583e8ba8dc083e88bda29c
AnatoliySt20 <strikun.a.p@gmail.com>
2017-10-11 18:05:34 +0300

    Bootstrap, user model, user creation with admin, images

--- meetings/people/models.py
+++ meetings/people/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=64, blank=True, null=True, default=None)
+    email = models.EmailField()
+    date_of_birth = models.DateField()
+    address = models.CharField(max_length=64, blank=True, null=True, default=None)
+    school = models.CharField(max_length=64, blank=True, null=True, default=None)
+    photography = models.ImageField(upload_to='personal_images/')
+
+    def __str__(self):
+        return "%s" % self.name
